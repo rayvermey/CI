8e08fac9a75133970aa658e57aa4234493e0938b
Ray Vermey <rayvermey@gmail.com>
2022-02-23 09:54:32 +0100

    improvements

--- install.sh
+++ install.sh
@@ -193,15 +193,15 @@ su ray -c "yay --noconfirm -R libxft xorg-x11perf"
 
 echo cleaning up
 
-#sed -i 's/Storage=volatile/#Storage=auto/' /etc/systemd/journald.conf
-#rm /etc/udev/rules.d/81-dhcpcd.rules
-#systemctl disable pacman-init.service choose-mirror.service
-#rm -r /etc/systemd/system/{choose-mirror.service,pacman-init.service,etc-pacman.d-gnupg.mount,getty@tty1.service.d}
-#rm /etc/systemd/scripts/choose-mirror
-#rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
-#rm /root/{.automated_script.sh,.zlogin}
-#rm /etc/mkinitcpio-archiso.conf
-#rm -r /etc/initcpio
+sed -i 's/Storage=volatile/#Storage=auto/' /etc/systemd/journald.conf
+rm /etc/udev/rules.d/81-dhcpcd.rules
+systemctl disable pacman-init.service choose-mirror.service
+rm -r /etc/systemd/system/{choose-mirror.service,pacman-init.service,etc-pacman.d-gnupg.mount,getty@tty1.service.d}
+rm /etc/systemd/scripts/choose-mirror
+rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
+rm /root/{.automated_script.sh,.zlogin}
+rm /etc/mkinitcpio-archiso.conf
+rm -r /etc/initcpio
 
 echo mkinitcpio
 
