f24840a18019d8d00698c093c9394ce658de3ff1
Ray Vermey <rayvermey@gmail.com>
2022-07-18 10:27:47 +0200

    VM KDE Setup

---
+++ VM_KDE_SETUP/install_software.sh
@@ -0,0 +1,27 @@
+echo Installing needed software
+sudo pacman -S --noconfirm --needed ferdium-bin
+yay -S --noconfirm --needed autokey-gtk 
+yay -S --noconfirm --needed flameshot
+yay -S --noconfirm --needed vivaldi
+
+/usr/local/bin/data/tozsh
+/usr/local/bin/data/kvm
+
+echo Setting up rclone mounts
+
+sudo mkdir -p /DATA/cloud/Jotta
+sudo mkdir -p /DATA/cloud/Google_Drive
+sudo mkdir -p /DATA/SHARED
+sudo mkdir -p /MEDIA/Jotta_photos
+
+sudo cp -r etc/* /etc/systemd/system
+
+mkdir ~/.config/rclone
+cp .config/rclone.conf ~/.config/rclone
+
+systemctl enable --now rclone-mount-google_drive
+systemctl enable --now rclone-mount-photos
+systemctl enable --now rclone-mount
+systemctl enable --now rclone-mount-shared
+
+
