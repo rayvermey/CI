f4c7d344b2ad8929b8ed967d63c81440801ce5bd
Ray Vermey <rayvermey@gmail.com>
2022-10-11 15:57:26 +0200

    added rem-kde.sh

---
+++ rem-kde.sh
@@ -0,0 +1,2 @@
+sudo pacman -Rns plasma plasma-integration plasma-workspace kf5 kde-applications latte-dock-git lightly-git kwin-effects-burn-my-windows-git kwin-effects-sliding-notifications kde-dev-utils kdepim-runtime   kwin-bismuth-bin kdelibs4support ksysguard kwin-scripts-forceblur systemd-kcm systemdgenie pamac-tray-icon-plasma xdg-desktop-portal flatpak flatseal libpamac-flatpak-plugin
+
