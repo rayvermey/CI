f7ba4e1d511755cc0a958998d77cc77f0444571f
Ray Vermey <rayvermey@gmail.com>
2022-10-31 16:56:25 +0100

    cp instead of sed

--- post_install.sh
+++ post_install.sh
@@ -1,31 +1,31 @@
+sudo pacman -Syy
+
 if [ $(type dusk >/dev/null 2>&1) ] 
 then
 	echo Dusk installed
 else
 	echo "Installing Dusk & Picom & st & Variety & Feh and right variety script"
 	sudo pacman --needed -S picom variety feh yajl dmenu
-#	sed 's/"i3"/"i3" "dusk" /'  .config/variety/scripts/set_wallpaper > .config/variety/scripts/set_wallpaper.new
-	cp /etc/xdg/picom.conf ~/.config/picom
-	sed 's/"vsync = true;"/"vsync = false;"/' .config/picom/pioom.conf > .config/picom/pioom.conf.new 
-	mv .config/picom/pioom.conf.new  .config/picom/pioom.conf
-#	mv .config/variety/scripts/set_wallpaper.new > .config/variety/scripts/set_wallpaper
-	mkdir -p ~/.config/picom
+
+	mkdir ~/.config/picom
+	cp picom.conf  ~/.config/picom/picom.conf
+	cp set_wallpaper.new ~/.config/variety/scripts/set_wallpaper
+	sudo cp kde_settings.conf /etc/sddm.conf.d/kde_settings.conf
 	mkdir ~/git
 	cd ~/git
-	#git clone https://github.com/bakkeby/dusk
-	#git clone https://github.com/bakkeby/st-flexipatch
+	git clone https://github.com/bakkeby/dusk
+	git clone https://github.com/bakkeby/st-flexipatch
 	echo Making Dusk
 	cd ~/git/dusk
-	#make && sudo make install
+	make && sudo make install
 	echo Making st
 	cd ~/git/st-flexipatch
-	#make && sudo make install
-	echo Creating .xinitrc
-	cat <<XINITRC > ~/.xinitrc
-	xrandr -s 1920x1080 &
-	picom &
-	variety &
-	exec dusk
+	make && sudo make install
+	echo Creating .xprofile
+	cat <<XINITRC > ~/.xprofile
+xrandr -s 1920x1080 &
+picom &
+variety &
 XINITRC
 	
 fi
